3ee7fbda973ce4d72be1b80e9f3d81afa37ade35
Alexander Medeiros <ajmed13@gmail.com>
2017-12-22 18:34:09 -0500

    backing up, changing workstations

---
+++ validators.js
@@ -0,0 +1,31 @@
+const WidgetValidator = {
+  validate: function(widget, widgetAttributes, widgetCategoryOptions) {
+    if (!widget) {
+      return 'Failed validation: widget was falsy'
+    } else if (!widgetAttributes) {
+      return 'Failed validation: widgetAttributes was falsy'
+    } else if (!widgetCategoryOptions) {
+      return 'Failed validation: widgetCategoryOptions was falsy'
+    }
+
+    return 'pass'
+    // ensure all the attributes and categoryOptions are valid
+  }
+}
+
+const WidgetsRUsUserValidator = {
+  validate: function(widgetsRUsUser) {
+    if (!widgetsRUsUser.username) {
+      return 'Failed validation: username was falsy'
+    } else if (!widgetsRUsUser.username.match(/^[a-z0-9_-]{3,15}$/)) {
+      return 'Failed validation: Invalid username, must be between 3 & 15 characters and may contain only letters, numbers, and hyphens'
+    }
+
+    return 'pass'
+  }
+}
+
+module.exports = {
+  WidgetValidator: WidgetValidator,
+  WidgetsRUsUserValidator: WidgetsRUsUserValidator
+}
